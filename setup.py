dc26caf2583169c1944350df6af6ec60a1cdd83c
Chris Bednarski <banzaimonkey@gmail.com>
2016-03-24 10:11:19 -0700

    Added version 0.2.1 with trove classification

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='icecake',
-    version='0.2.0',
+    version='0.2.1',
     py_modules=['icecake', 'templates'],
     url="https://github.com/cbednarski/icecake",
     author="Chris Bednarski",
     author_email="banzaimonkey@gmail.com",
     description="An easy and cool static site generator",
     license="MIT",
+    long_description=open('README.rst').read(),
     install_requires=[
         'Click',
         'Jinja2',
@@ -21,5 +22,14 @@ setup(
         [console_scripts]
         icecake=icecake:cli
     ''',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ],
     keywords="static site generator builder icecake"
 )
