f392783229826ebf7b2b3f89426f7a3e12a9aedc
Chris Bednarski <banzaimonkey@gmail.com>
2016-04-04 01:24:32 -0700

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='icecake',
-    version='0.3.4',
+    version='0.4.0',
     py_modules=['icecake', 'templates'],
     url="https://github.com/cbednarski/icecake",
     author="Chris Bednarski",
